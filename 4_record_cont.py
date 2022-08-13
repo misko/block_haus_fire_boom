e97f98c690b292739a9bdf1991541b9d0afe58d9
misko <misko@cs.toronto.edu>
2022-08-13 14:36:07 -0700

    first commit, mostly copied code

---
+++ 4_record_cont.py
@@ -0,0 +1,39 @@
+import pyaudio
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+form_1 = pyaudio.paInt16 # 16-bit resolution
+chans = 1 # 1 channel
+samp_rate = 44100 # 44.1kHz sampling rate
+chunk = 8192 # 2^12 samples for buffer
+dev_index = 2 # device index found by p.get_device_info_by_index(ii)
+
+# mic sensitivity correction and bit conversion
+mic_sens_dBV = -47.0 # mic sensitivity in dBV + any gain
+mic_sens_corr = np.power(10.0,mic_sens_dBV/20.0) # calculate mic sensitivity conversion factor
+
+
+audio = pyaudio.PyAudio() # create pyaudio instantiation
+
+# create pyaudio stream
+stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                    input_device_index = dev_index,input = True, \
+                    frames_per_buffer=chunk)
+
+# record data chunk 
+stream.start_stream()
+frames = [ stream.read(chunk),dtype=np.int16) for x in range(10) ]
+
+
+stream.stop_stream()
+
+import wave
+wav_output_filename = 'test1.wav' # name of .wav file
+# save the audio frames as .wav file
+wavefile = wave.open(wav_output_filename,'wb')
+wavefile.setnchannels(chans)
+wavefile.setsampwidth(audio.get_sample_size(form_1))
+wavefile.setframerate(samp_rate)
+wavefile.writeframes(b''.join(frames))
+wavefile.close()
---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+sudo pip3 install pyaudio
+pip3 install matplotlib
+pip install numpy --upgrade
+sudo apt-get install libatlas-base-dev
